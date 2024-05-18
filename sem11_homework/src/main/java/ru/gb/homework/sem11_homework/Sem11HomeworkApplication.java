5765bf351366d7a67359e92d288e765cd9e8d6ed
ilya.devel <ilya.devel@yandex.ru>
2024-05-18 17:53:05 +0500

    sem11_homework completed

---
+++ sem11_homework/src/main/java/ru/gb/homework/sem11_homework/Sem11HomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.homework.sem11_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem11HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem11HomeworkApplication.class, args);
+	}
+
+}
---
+++ sem11_homework/src/test/java/ru/gb/homework/sem11_homework/Sem11HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.homework.sem11_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem11HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
